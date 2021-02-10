ec9af0236cd621fda9f9ceed3ae2c18f4903fc1d
John-Li-happy <63318597+John-Li-happy@users.noreply.github.com>
2021-02-09 19:29:35 -0500

    init commit

---
+++ JLProcesser/JLProcesser/JLProcesser.h
@@ -0,0 +1,18 @@
+//
+//  JLProcesser.h
+//  JLProcesser
+//
+//  Created by Zhaoyang Li on 2/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JLProcesser.
+FOUNDATION_EXPORT double JLProcesserVersionNumber;
+
+//! Project version string for JLProcesser.
+FOUNDATION_EXPORT const unsigned char JLProcesserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JLProcesser/PublicHeader.h>
+
+
---
+++ JLProcesser/JLProcesser/JLProcesserString.swift
@@ -0,0 +1,17 @@
+//
+//  JLProcesserString.swift
+//  JLProcesser
+//
+//  Created by Zhaoyang Li on 2/9/21.
+//
+
+import Foundation
+
+public class JLProcesserString {
+    private init() {}
+    
+    public static func initString() -> String {
+        return "this project has been init correctly"
+    }
+}
+
