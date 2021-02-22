6e05111f449362106252c77f205810997bcabb45
John-Li-happy <63318597+John-Li-happy@users.noreply.github.com>
2021-02-22 17:45:44 -0500

    another trial of updating

---
+++ JLProcesser/JLProcesserString.swift
@@ -0,0 +1,16 @@
+//
+//  JLProcesserString.swift
+//  JLProcesser
+//
+//  Created by Zhaoyang Li on 2/9/21.
+//
+
+import Foundation
+
+public class JLProcesserString {
+    private init() {}
+    
+    public static func initString() -> String {
+        return "this project has been init correctly"
+    }
+}
